1cf5a5de4b1d7042f65096cd6a430aeb0d49a5db
Kaytiue <volkan@moriayazilim.com>
2025-07-31 21:13:28 +0300

    deneme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex items-center justify-center text-black">
             <div className='border-t border-black py-6 w-1/2 items-center justify-center flex'>
-                <p className='md:text-lg text-md'>Moria Yazılım © 2025</p>
+                <p className='md:text-lg text-md'>Moria Yazılım</p>
             </div>
         </footer>
     )
